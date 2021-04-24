ab1eade6f106ea90aadbc7f6090ea388d7011c75
Ada Cooke <me@ada.rip>
2021-04-24 16:18:34 +0100

    fix challenge submit

--- polaris-cli/src/main/java/uk/co/ractf/polaris/cli/subcommands/Challenge.java
+++ polaris-cli/src/main/java/uk/co/ractf/polaris/cli/subcommands/Challenge.java
@@ -1,12 +1,16 @@
 package uk.co.ractf.polaris.cli.subcommands;
 
 import picocli.CommandLine;
+import uk.co.ractf.polaris.cli.subcommands.challenge.ChallengeDelete;
 import uk.co.ractf.polaris.cli.subcommands.challenge.ChallengeList;
+import uk.co.ractf.polaris.cli.subcommands.challenge.ChallengeSubmit;
 
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "challenge", aliases = {"chal", "ch"}, subcommands = {
-        ChallengeList.class
+        ChallengeList.class,
+        ChallengeDelete.class,
+        ChallengeSubmit.class
 })
 public class Challenge implements Callable<Integer> {
     @Override
