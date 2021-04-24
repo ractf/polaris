e27b65f998f621d79664d6210442cd99f2cdde5f
Ada Cooke <me@ada.rip>
2021-04-24 11:30:17 +0100

    handle authentication, add ping endpoint

---
+++ polaris-cli/src/main/java/uk/co/ractf/polaris/cli/APIClientFactory.java
@@ -0,0 +1,12 @@
+package uk.co.ractf.polaris.cli;
+
+import uk.co.ractf.polaris.apiclient.APIClient;
+
+public class APIClientFactory {
+
+    public static APIClient createAPIClient() {
+        final CLIConfig cliConfig = CLIConfig.readFromFile();
+        return APIClient.create(cliConfig.getHost(), cliConfig.getUsername(), cliConfig.getPassword());
+    }
+
+}
---
+++ polaris-cli/src/main/java/uk/co/ractf/polaris/cli/CLIConfig.java
@@ -0,0 +1,60 @@
+package uk.co.ractf.polaris.cli;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import uk.co.ractf.polaris.api.common.JsonRepresentable;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CLIConfig extends JsonRepresentable {
+
+    private final String host;
+    private final String username;
+    private final String password;
+
+    public CLIConfig(
+            @JsonProperty("host") final String host,
+            @JsonProperty("username") final String username,
+            @JsonProperty("password") final String password) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static boolean doesFileExist() {
+        return Path.of(System.getProperty("user.home"), ".polaris", "config.json").toFile().exists();
+    }
+
+    public static CLIConfig readFromFile() {
+        try {
+            return parse(new String(Files.readAllBytes(Path.of(System.getProperty("user.home"), ".polaris", "config.json"))), CLIConfig.class);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void save() {
+        Path.of(System.getProperty("user.home"), ".polaris").toFile().mkdirs();
+        try {
+            Files.writeString(Path.of(System.getProperty("user.home"), ".polaris", "config.json"), toJsonString());
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
