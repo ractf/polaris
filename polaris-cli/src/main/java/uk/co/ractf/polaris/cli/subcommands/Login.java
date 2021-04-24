9dec2a92024f4dd04ffc89438cf874f9d7a5a7c9
Ada Cooke <me@ada.rip>
2021-04-24 09:08:44 +0100

    add api client to gradle

--- polaris-cli/src/main/java/uk/co/ractf/polaris/cli/subcommands/Login.java
+++ polaris-cli/src/main/java/uk/co/ractf/polaris/cli/subcommands/Login.java
@@ -7,15 +7,26 @@ import uk.co.ractf.polaris.cli.Subcommand;
 public class Login extends Subcommand {
 
     @CommandLine.Option(names = {"-u", "--user"}, description = "Username")
-    String user;
+    private String user;
 
-    @CommandLine.Option(names = {"-p", "--password"}, description = "Password", interactive = true)
-    char[] password;
+    @CommandLine.Option(names = {"-p", "--password"}, description = "Password")
+    private String password;
+
+    @CommandLine.Parameters(index = "0")
+    private String host;
+
+    @CommandLine.Option(names = {"-P", "--port"}, defaultValue = "8080", description = "Port")
+    private int port;
 
     @Override
     public int run() throws Exception {
-        System.out.println(user);
-        System.out.println(password);
+        if (user == null) {
+            System.out.printf("Login to %s%n", host);
+            user = System.console().readLine("Username: ");
+        }
+        if (password == null) {
+            password = String.valueOf(System.console().readPassword("Password: "));
+        }
         return 0;
     }
 
