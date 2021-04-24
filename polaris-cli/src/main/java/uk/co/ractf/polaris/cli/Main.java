6bdc9c15b8d2bbcd9a2a57bab0fdb433e642f68e
Ada Cooke <me@ada.rip>
2021-04-24 08:21:49 +0100

    native image

--- polaris-cli/src/main/java/uk/co/ractf/polaris/cli/Main.java
+++ polaris-cli/src/main/java/uk/co/ractf/polaris/cli/Main.java
@@ -1,9 +1,22 @@
 package uk.co.ractf.polaris.cli;
 
-public class Main {
+import picocli.CommandLine;
+import uk.co.ractf.polaris.api.challenge.Challenge;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command()
+public class Main implements Callable<Integer> {
 
     public static void main(final String[] args) {
-
+        final int exitCode = new CommandLine(new Main()).execute(args);
+        System.out.println(new Challenge("aa", new ArrayList<>()).toJsonString());
+        System.exit(exitCode);
     }
 
+    @Override
+    public Integer call() throws Exception {
+        return 69;
+    }
 }
