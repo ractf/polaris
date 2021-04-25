61ee890fbb9030f7f1f7de2b716c56abaeb7f6b0
Ada Cooke <me@ada.rip>
2021-04-25 09:34:26 +0100

    host test

---
+++ polaris-api/src/test/java/uk/co/ractf/polaris/api/HostTestCase.java
@@ -0,0 +1,22 @@
+package uk.co.ractf.polaris.api;
+
+import org.junit.jupiter.api.Test;
+import uk.co.ractf.polaris.api.host.HostInfo;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static uk.co.ractf.polaris.testlib.JacksonTester.validateObject;
+import static uk.co.ractf.polaris.testlib.JacksonTester.validateObjectIgnoreProperties;
+
+public class HostTestCase {
+
+    @Test
+    public void testHostInfo() {
+        validateObject(HostInfo.class, fixture("fixtures/host/info.json"));
+    }
+
+    @Test
+    public void testHostInfoIgnoreProperties() {
+        validateObjectIgnoreProperties(HostInfo.class, fixture("fixtures/host/info.json"));
+    }
+
+}
