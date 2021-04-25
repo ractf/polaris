b1e48e740bea03c054c3cca929183a741902642c
Ada Cooke <me@ada.rip>
2021-04-25 08:55:45 +0100

    make andromeda api tests pass

--- polaris-api/src/main/java/uk/co/ractf/polaris/api/andromeda/AndromedaChallengeSubmitResponse.java
+++ polaris-api/src/main/java/uk/co/ractf/polaris/api/andromeda/AndromedaChallengeSubmitResponse.java
@@ -1,11 +1,13 @@
 package uk.co.ractf.polaris.api.andromeda;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Deprecated
 public class AndromedaChallengeSubmitResponse {
 
     private final String id;
 
-    public AndromedaChallengeSubmitResponse(final String id) {
+    public AndromedaChallengeSubmitResponse(@JsonProperty("id") final String id) {
         this.id = id;
     }
 
