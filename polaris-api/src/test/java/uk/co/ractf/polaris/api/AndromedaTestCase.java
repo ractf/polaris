d7d670e887c05e0f1be475bdc522a5e9d63c0bc4
David Cooke <me@dave.lc>
2021-04-25 09:00:05 +0100

    test for @JsonIgnoreProperties(ignoreUnknown = true) being present

--- polaris-api/src/test/java/uk/co/ractf/polaris/api/AndromedaTestCase.java
+++ polaris-api/src/test/java/uk/co/ractf/polaris/api/AndromedaTestCase.java
@@ -4,38 +4,69 @@ import org.junit.jupiter.api.Test;
 import uk.co.ractf.polaris.api.andromeda.*;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
-import static uk.co.ractf.polaris.testlib.JacksonTester.testObject;
+import static uk.co.ractf.polaris.testlib.JacksonTester.validateObject;
+import static uk.co.ractf.polaris.testlib.JacksonTester.validateObjectIgnoreProperties;
 
 public class AndromedaTestCase {
 
     @Test
     public void testAndromedaAuthentication() {
-        testObject(AndromedaAuthentication.class, fixture("fixtures/andromeda/authentication.json"));
+        validateObject(AndromedaAuthentication.class, fixture("fixtures/andromeda/authentication.json"));
     }
 
     @Test
     public void testAndromedaChallenge() {
-        testObject(AndromedaChallenge.class, fixture("fixtures/andromeda/challenge.json"));
+        validateObject(AndromedaChallenge.class, fixture("fixtures/andromeda/challenge.json"));
     }
 
     @Test
     public void testInstance() {
-        testObject(AndromedaInstance.class, fixture("fixtures/andromeda/instance.json"));
+        validateObject(AndromedaInstance.class, fixture("fixtures/andromeda/instance.json"));
     }
 
     @Test
     public void testInstanceRequest() {
-        testObject(AndromedaInstanceRequest.class, fixture("fixtures/andromeda/instancerequest.json"));
+        validateObject(AndromedaInstanceRequest.class, fixture("fixtures/andromeda/instancerequest.json"));
     }
 
     @Test
     public void testResources() {
-        testObject(AndromedaResources.class, fixture("fixtures/andromeda/resources.json"));
+        validateObject(AndromedaResources.class, fixture("fixtures/andromeda/resources.json"));
     }
 
     @Test
     public void testSubmitResponse() {
-        testObject(AndromedaChallengeSubmitResponse.class, fixture("fixtures/andromeda/submitresponse.json"));
+        validateObject(AndromedaChallengeSubmitResponse.class, fixture("fixtures/andromeda/submitresponse.json"));
+    }
+
+    @Test
+    public void testAndromedaAuthenticationIgnoreProperties() {
+        validateObjectIgnoreProperties(AndromedaAuthentication.class, fixture("fixtures/andromeda/authentication.json"));
+    }
+
+    @Test
+    public void testAndromedaChallengeIgnoreProperties() {
+        validateObjectIgnoreProperties(AndromedaChallenge.class, fixture("fixtures/andromeda/challenge.json"));
+    }
+
+    @Test
+    public void testInstanceIgnoreProperties() {
+        validateObjectIgnoreProperties(AndromedaInstance.class, fixture("fixtures/andromeda/instance.json"));
+    }
+
+    @Test
+    public void testInstanceRequestIgnoreProperties() {
+        validateObjectIgnoreProperties(AndromedaInstanceRequest.class, fixture("fixtures/andromeda/instancerequest.json"));
+    }
+
+    @Test
+    public void testResourcesIgnoreProperties() {
+        validateObjectIgnoreProperties(AndromedaResources.class, fixture("fixtures/andromeda/resources.json"));
+    }
+
+    @Test
+    public void testSubmitResponseIgnoreProperties() {
+        validateObjectIgnoreProperties(AndromedaChallengeSubmitResponse.class, fixture("fixtures/andromeda/submitresponse.json"));
     }
 
 }
--- polaris-testlib/src/main/java/uk/co/ractf/polaris/testlib/JacksonTester.java
+++ polaris-testlib/src/main/java/uk/co/ractf/polaris/testlib/JacksonTester.java
@@ -1,7 +1,10 @@
 package uk.co.ractf.polaris.testlib;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,9 +12,21 @@ public class JacksonTester {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static <T> void testObject(final Class<T> clazz, final String fixture) {
+    public static <T> void validateObject(final Class<T> clazz, final String fixture) {
         try {
             final T t = objectMapper.readValue(fixture, clazz);
+            final String result = objectMapper.writeValueAsString(t);
+            assertEquals(objectMapper.readTree(result), objectMapper.readTree(fixture));
+        } catch (final JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> void validateObjectIgnoreProperties(final Class<T> clazz, final String fixture) {
+        try {
+            final Map<String, Object> map = objectMapper.readValue(fixture, new TypeReference<>() {});
+            map.put("UNKNOWN_PROPERTY_KEY", "UNKNOWN_PROPERTY_VALUE");
+            final T t = objectMapper.readValue(objectMapper.writeValueAsString(map), clazz);
             final String result = objectMapper.writeValueAsString(t);
             assertEquals(objectMapper.readTree(result), objectMapper.readTree(fixture));
         } catch (final JsonProcessingException e) {
