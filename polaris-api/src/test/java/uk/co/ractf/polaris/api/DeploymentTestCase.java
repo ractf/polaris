57a6c70ca86416e9b1afa30ef354fb00a1b0393f
David Cooke <me@dave.lc>
2021-04-25 09:17:08 +0100

    deployment test

---
+++ polaris-api/src/test/java/uk/co/ractf/polaris/api/DeploymentTestCase.java
@@ -0,0 +1,44 @@
+package uk.co.ractf.polaris.api;
+
+import org.junit.jupiter.api.Test;
+import uk.co.ractf.polaris.api.deployment.Allocation;
+import uk.co.ractf.polaris.api.deployment.Deployment;
+import uk.co.ractf.polaris.api.deployment.StaticReplication;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static uk.co.ractf.polaris.testlib.JacksonTester.validateObject;
+import static uk.co.ractf.polaris.testlib.JacksonTester.validateObjectIgnoreProperties;
+
+public class DeploymentTestCase {
+
+    @Test
+    public void testAllocation() {
+        validateObject(Allocation.class, fixture("fixtures/deployment/allocation.json"));
+    }
+
+    @Test
+    public void testDeployment() {
+        validateObject(Deployment.class, fixture("fixtures/deployment/deployment.json"));
+    }
+
+    @Test
+    public void testStaticReplication() {
+        validateObject(StaticReplication.class, fixture("fixtures/deployment/staticreplication.json"));
+    }
+
+    @Test
+    public void testAllocationIgnoreProperties() {
+        validateObjectIgnoreProperties(Allocation.class, fixture("fixtures/deployment/allocation.json"));
+    }
+
+    @Test
+    public void testDeploymentIgnoreProperties() {
+        validateObjectIgnoreProperties(Deployment.class, fixture("fixtures/deployment/deployment.json"));
+    }
+
+    @Test
+    public void testStaticReplicationIgnoreProperties() {
+        validateObjectIgnoreProperties(StaticReplication.class, fixture("fixtures/deployment/staticreplication.json"));
+    }
+
+}
