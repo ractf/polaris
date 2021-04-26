152cf2bbcba475c63a57cbb1e5f7e714c1411840
David Cooke <me@dave.lc>
2021-04-26 08:24:22 +0100

    .equals test for deployment definitions

--- polaris-api/src/test/java/uk/co/ractf/polaris/api/DeploymentTestCase.java
+++ polaris-api/src/test/java/uk/co/ractf/polaris/api/DeploymentTestCase.java
@@ -1,5 +1,6 @@
 package uk.co.ractf.polaris.api;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 import uk.co.ractf.polaris.api.deployment.Allocation;
 import uk.co.ractf.polaris.api.deployment.Deployment;
@@ -39,6 +40,21 @@ public class DeploymentTestCase {
     @Test
     public void testStaticReplicationIgnoreProperties() {
         validateObjectIgnoreProperties(StaticReplication.class, fixture("fixtures/deployment/staticreplication.json"));
+    }
+
+    @Test
+    public void testAllocationEquals() {
+        EqualsVerifier.simple().forClass(Allocation.class).verify();
+    }
+
+    @Test
+    public void testDeploymentEquals() {
+        EqualsVerifier.simple().forClass(Deployment.class).verify();
+    }
+
+    @Test
+    public void testStaticReplicationEquals() {
+        EqualsVerifier.simple().forClass(StaticReplication.class).verify();
     }
 
 }
