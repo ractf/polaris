2a1c496410138e79dd0f26fc9e9effb6ee5c701f
David Cooke <me@dave.lc>
2021-04-26 11:51:13 +0100

    server -> controller

---
+++ polaris-controller/src/main/java/uk/co/ractf/polaris/Main.java
@@ -0,0 +1,20 @@
+package uk.co.ractf.polaris;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(final String[] args) throws Exception {
+        if (args.length == 0) {
+            System.out.println("Usage: java -jar polaris.jar (controller/node)");
+            System.exit(1);
+        }
+
+        if (args[0].equals("controller")) {
+            ControllerMain.main(Arrays.copyOfRange(args, 1, args.length));
+        } else if (args[1].equals("node")) {
+            NodeMain.main(Arrays.copyOfRange(args, 1, args.length));
+        }
+    }
+
+}
