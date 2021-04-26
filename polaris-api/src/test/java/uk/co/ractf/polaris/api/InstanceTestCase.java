83b4f56a755beb349d4d29e8cd34fe3101871cab
David Cooke <me@dave.lc>
2021-04-26 09:57:14 +0100

    100% coverage on instance

--- polaris-api/src/test/java/uk/co/ractf/polaris/api/InstanceTestCase.java
+++ polaris-api/src/test/java/uk/co/ractf/polaris/api/InstanceTestCase.java
@@ -1,11 +1,13 @@
 package uk.co.ractf.polaris.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 import uk.co.ractf.polaris.api.instance.Instance;
 import uk.co.ractf.polaris.api.instance.InstancePortBinding;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
 import static uk.co.ractf.polaris.testlib.JacksonTester.validateObject;
 import static uk.co.ractf.polaris.testlib.JacksonTester.validateObjectIgnoreProperties;
 
@@ -39,6 +41,13 @@ public class InstanceTestCase {
     @Test
     public void testPortEquals() {
         EqualsVerifier.simple().forClass(InstancePortBinding.class).verify();
+    }
+
+    @Test
+    public void testAddPortBinding() throws JsonProcessingException {
+        final Instance instance = Instance.parse(fixture("fixtures/instance/instance.json"), Instance.class);
+        instance.addPortBinding(new InstancePortBinding("69", "0.0.0.0", true));
+        assertThat(instance.getPortBindings()).size().isEqualTo(1);
     }
 
 }
