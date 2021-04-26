07be11f75380dd2e973c32aaf6cd5880f96f3fea
Ada Cooke <me@ada.rip>
2021-04-26 08:52:04 +0100

    instance request/response equals test

--- polaris-api/src/test/java/uk/co/ractf/polaris/api/InstanceAllocationTestCase.java
+++ polaris-api/src/test/java/uk/co/ractf/polaris/api/InstanceAllocationTestCase.java
@@ -1,5 +1,6 @@
 package uk.co.ractf.polaris.api;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 import uk.co.ractf.polaris.api.instanceallocation.InstanceRequest;
 import uk.co.ractf.polaris.api.instanceallocation.InstanceResponse;
@@ -14,17 +15,29 @@ public class InstanceAllocationTestCase {
     public void testInstanceRequest() {
         validateObject(InstanceRequest.class, fixture("fixtures/instanceallocation/request.json"));
     }
+
     @Test
     public void testInstanceResponse() {
         validateObject(InstanceResponse.class, fixture("fixtures/instanceallocation/response.json"));
     }
+
     @Test
     public void testInstanceRequestIgnoreProperties() {
         validateObjectIgnoreProperties(InstanceRequest.class, fixture("fixtures/instanceallocation/request.json"));
     }
+
     @Test
     public void testInstanceResponseIgnoreProperties() {
         validateObjectIgnoreProperties(InstanceResponse.class, fixture("fixtures/instanceallocation/response.json"));
     }
 
+    @Test
+    public void testInstanceRequestEquals() {
+        EqualsVerifier.simple().forClass(InstanceRequest.class).verify();
+    }
+
+    @Test
+    public void testInstanceResponseEquals() {
+        EqualsVerifier.simple().forClass(InstanceResponse.class).verify();
+    }
 }
