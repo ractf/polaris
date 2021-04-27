2a7d5aeb823ea4298f7d80658301e8651007dc7e
David Cooke <me@dave.lc>
2021-04-27 15:48:19 +0100

    deployment submit command

--- polaris-cli/src/main/java/uk/co/ractf/polaris/cli/subcommands/Deployment.java
+++ polaris-cli/src/main/java/uk/co/ractf/polaris/cli/subcommands/Deployment.java
@@ -1,11 +1,12 @@
 package uk.co.ractf.polaris.cli.subcommands;
 
 import picocli.CommandLine;
+import uk.co.ractf.polaris.cli.subcommands.deployment.DeploymentSubmit;
 
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "deployment", aliases = {"d"}, subcommands = {
-
+        DeploymentSubmit.class
 })
 public class Deployment implements Callable<Integer> {
     @Override
