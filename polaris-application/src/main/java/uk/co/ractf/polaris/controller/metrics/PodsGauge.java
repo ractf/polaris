670a2caa7a4441f195ea03b6e14aec75ebb86a0b
Ada Cooke <me@ada.rip>
2021-04-27 20:11:09 +0100

    pods gauge

---
+++ polaris-application/src/main/java/uk/co/ractf/polaris/controller/metrics/PodsGauge.java
@@ -0,0 +1,26 @@
+package uk.co.ractf.polaris.controller.metrics;
+
+import com.codahale.metrics.Gauge;
+import uk.co.ractf.polaris.api.challenge.Challenge;
+import uk.co.ractf.polaris.state.ClusterState;
+
+import java.util.Map;
+
+public class PodsGauge implements Gauge<Integer> {
+
+    private final ClusterState clusterState;
+
+    public PodsGauge(final ClusterState clusterState) {
+        this.clusterState = clusterState;
+    }
+
+    @Override
+    public Integer getValue() {
+        int total = 0;
+        for (final Map.Entry<String, Challenge> entry : clusterState.getChallenges().entrySet()) {
+            total += entry.getValue().getPods().size();
+        }
+
+        return total;
+    }
+}
