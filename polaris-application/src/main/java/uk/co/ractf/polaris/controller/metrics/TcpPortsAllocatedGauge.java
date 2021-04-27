1caff4cf4cd1ff1c83d96db635e568b5d4bc55de
David Cooke <me@dave.lc>
2021-04-27 20:01:46 +0100

    tcp ports allocated gauge

---
+++ polaris-application/src/main/java/uk/co/ractf/polaris/controller/metrics/TcpPortsAllocatedGauge.java
@@ -0,0 +1,26 @@
+package uk.co.ractf.polaris.controller.metrics;
+
+import com.codahale.metrics.Gauge;
+import uk.co.ractf.polaris.api.node.NodeInfo;
+import uk.co.ractf.polaris.state.ClusterState;
+
+import java.util.Map;
+
+public class TcpPortsAllocatedGauge implements Gauge<Integer> {
+
+    private final ClusterState clusterState;
+
+    public TcpPortsAllocatedGauge(final ClusterState clusterState) {
+        this.clusterState = clusterState;
+    }
+
+    @Override
+    public Integer getValue() {
+        int total = 0;
+        for (final Map.Entry<String, NodeInfo> entry : clusterState.getNodes().entrySet()) {
+            total += entry.getValue().getPortAllocations().getTcp().size();
+        }
+
+        return total;
+    }
+}
