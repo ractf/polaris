01f43b8cb87de7ec2a090d281dfcc2beb01b14b3
David Cooke <me@dave.lc>
2021-04-27 19:48:41 +0100

    memory used gauge

---
+++ polaris-application/src/main/java/uk/co/ractf/polaris/controller/metrics/MemoryUsedGauge.java
@@ -0,0 +1,26 @@
+package uk.co.ractf.polaris.controller.metrics;
+
+import com.codahale.metrics.Gauge;
+import uk.co.ractf.polaris.api.node.NodeInfo;
+import uk.co.ractf.polaris.state.ClusterState;
+
+import java.util.Map;
+
+public class MemoryUsedGauge implements Gauge<Long> {
+
+    private final ClusterState clusterState;
+
+    public MemoryUsedGauge(final ClusterState clusterState) {
+        this.clusterState = clusterState;
+    }
+
+    @Override
+    public Long getValue() {
+        long total = 0;
+        for (final Map.Entry<String, NodeInfo> entry : clusterState.getNodes().entrySet()) {
+            total += entry.getValue().getTotalMemory() - entry.getValue().getFreeMemory();
+        }
+
+        return total;
+    }
+}
