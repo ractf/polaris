d2bc5488212a84b487527d5b7bc6437ebc6cdfc2
David Cooke <me@dave.lc>
2021-04-27 19:54:38 +0100

    cpu used gauge

---
+++ polaris-application/src/main/java/uk/co/ractf/polaris/controller/metrics/CpuUsedGauge.java
@@ -0,0 +1,26 @@
+package uk.co.ractf.polaris.controller.metrics;
+
+import com.codahale.metrics.Gauge;
+import uk.co.ractf.polaris.api.node.NodeInfo;
+import uk.co.ractf.polaris.state.ClusterState;
+
+import java.util.Map;
+
+public class CpuUsedGauge implements Gauge<Double> {
+
+    private final ClusterState clusterState;
+
+    public CpuUsedGauge(final ClusterState clusterState) {
+        this.clusterState = clusterState;
+    }
+
+    @Override
+    public Double getValue() {
+        double total = 0;
+        for (final Map.Entry<String, NodeInfo> entry : clusterState.getNodes().entrySet()) {
+            total += entry.getValue().getCpuLoad();
+        }
+
+        return total;
+    }
+}
