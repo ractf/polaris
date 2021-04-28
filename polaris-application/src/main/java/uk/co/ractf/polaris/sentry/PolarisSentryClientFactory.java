24494bcf7c906b74f6b594c8622303925cf1daa8
David Cooke <me@dave.lc>
2021-04-28 15:11:01 +0100

    read the version from a file inside the jar to make errors nicer

--- polaris-application/src/main/java/uk/co/ractf/polaris/sentry/PolarisSentryClientFactory.java
+++ polaris-application/src/main/java/uk/co/ractf/polaris/sentry/PolarisSentryClientFactory.java
@@ -3,13 +3,14 @@ package uk.co.ractf.polaris.sentry;
 import io.sentry.DefaultSentryClientFactory;
 import io.sentry.SentryClient;
 import io.sentry.dsn.Dsn;
+import uk.co.ractf.polaris.util.PolarisVersion;
 
 public class PolarisSentryClientFactory extends DefaultSentryClientFactory {
 
     @Override
     public SentryClient createSentryClient(final Dsn dsn) {
         final SentryClient sentryClient = super.createSentryClient(dsn);
-        sentryClient.setRelease("@version@");
+        sentryClient.setRelease(PolarisVersion.VERSION);
         return sentryClient;
     }
 }
