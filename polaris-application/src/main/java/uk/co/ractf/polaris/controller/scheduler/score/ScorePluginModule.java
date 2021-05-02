20432bae213e4559c3836190ebe5f744cb8d2872
David Cooke <me@dave.lc>
2021-05-03 00:49:05 +0100

    cluster predicate scheduler plugins

---
+++ polaris-application/src/main/java/uk/co/ractf/polaris/controller/scheduler/score/ScorePluginModule.java
@@ -0,0 +1,14 @@
+package uk.co.ractf.polaris.controller.scheduler.score;
+
+import com.google.inject.multibindings.Multibinder;
+import ru.vyarus.dropwizard.guice.module.support.DropwizardAwareModule;
+import uk.co.ractf.polaris.controller.ControllerConfiguration;
+import uk.co.ractf.polaris.controller.scheduler.ScorePlugin;
+
+public class ScorePluginModule extends DropwizardAwareModule<ControllerConfiguration> {
+
+    @Override
+    protected void configure() {
+        final var multibinder = Multibinder.newSetBinder(binder(), ScorePlugin.class);
+    }
+}
