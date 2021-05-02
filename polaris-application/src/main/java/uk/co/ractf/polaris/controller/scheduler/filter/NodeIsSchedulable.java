a815a1fd94b2a14d97695af55fa362ec11086bbe
David Cooke <me@dave.lc>
2021-05-03 00:53:54 +0100

    NodeIsSchedulable

---
+++ polaris-application/src/main/java/uk/co/ractf/polaris/controller/scheduler/filter/NodeIsSchedulable.java
@@ -0,0 +1,23 @@
+package uk.co.ractf.polaris.controller.scheduler.filter;
+
+import uk.co.ractf.polaris.api.node.NodeInfo;
+import uk.co.ractf.polaris.api.task.Task;
+import uk.co.ractf.polaris.controller.scheduler.FilterPlugin;
+import uk.co.ractf.polaris.controller.scheduler.FilterResult;
+
+import java.util.Collections;
+
+public class NodeIsSchedulable implements FilterPlugin {
+    @Override
+    public FilterResult filter(final Task task, final NodeInfo nodeInfo) {
+        if (!nodeInfo.isSchedulable()) {
+            return new FilterResult(false, false, Collections.singletonList("Node is not schedulable"));
+        }
+        return FilterResult.SCHEDULABLE;
+    }
+
+    @Override
+    public String getName() {
+        return "NodeIsSchedulable";
+    }
+}
