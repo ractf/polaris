e0386a5247f2889f269b8b91154bbaeaabd713ea
Ada Cooke <me@ada.rip>
2021-05-03 04:09:55 +0100

    fix ping

--- polaris-application/src/main/java/uk/co/ractf/polaris/controller/resources/PingResource.java
+++ polaris-application/src/main/java/uk/co/ractf/polaris/controller/resources/PingResource.java
@@ -2,14 +2,16 @@ package uk.co.ractf.polaris.controller.resources;
 
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Timed;
-import io.dropwizard.auth.Auth;
 import io.swagger.v3.oas.annotations.Operation;
-import org.eclipse.jetty.server.Authentication;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import java.util.Map;
 
 @Path("/ping")
@@ -19,10 +21,12 @@ public class PingResource {
     @GET
     @Timed
     @ExceptionMetered
-    @Operation(summary = "Ping", tags = {"Ping"},
-            description = "Returns the current user's username")
-    public Map<String, String> ping(@Auth final Authentication.User user) {
-        return Map.of("username", user.getUserIdentity().getUserPrincipal().getName());
+    @Operation(summary = "Ping", tags = {"Ping"}, description = "Ping")
+    public Map<String, String> ping(@Context final SecurityContext context) {
+        if (context.getUserPrincipal() == null) {
+            throw new WebApplicationException(Response.Status.FORBIDDEN);
+        }
+        return Map.of("username", context.getUserPrincipal().getName());
     }
 
 }
