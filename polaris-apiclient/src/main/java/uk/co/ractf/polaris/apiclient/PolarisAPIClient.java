07a3dfd9e544ae357703af5f0426cb5d0377eb94
Ada Cooke <me@ada.rip>
2021-05-03 03:42:19 +0100

    task update command

--- polaris-apiclient/src/main/java/uk/co/ractf/polaris/apiclient/PolarisAPIClient.java
+++ polaris-apiclient/src/main/java/uk/co/ractf/polaris/apiclient/PolarisAPIClient.java
@@ -1,8 +1,8 @@
 package uk.co.ractf.polaris.apiclient;
 
-import uk.co.ractf.polaris.api.task.Challenge;
 import uk.co.ractf.polaris.api.task.Task;
-import uk.co.ractf.polaris.apiclient.command.*;
+import uk.co.ractf.polaris.apiclient.command.TaskCreateCommand;
+import uk.co.ractf.polaris.apiclient.command.TaskUpdateCommand;
 import uk.co.ractf.polaris.apiclient.transport.APIClientTransport;
 import uk.co.ractf.polaris.apiclient.transport.HttpClientTransport;
 
@@ -17,5 +17,10 @@ public class PolarisAPIClient implements APIClient {
     @Override
     public TaskCreateCommand createTask(final Task task) {
         return new TaskCreateCommand(transport, task);
+    }
+
+    @Override
+    public TaskUpdateCommand updateTask(final Task task) {
+        return new TaskUpdateCommand(transport, task);
     }
 }
