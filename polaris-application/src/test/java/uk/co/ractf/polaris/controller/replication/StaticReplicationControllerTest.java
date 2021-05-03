7236b3d23136d8c0c99394af64b65fce4b7c31a2
Ada Cooke <me@ada.rip>
2021-05-03 22:47:51 +0100

    dont need that annotation

--- polaris-application/src/test/java/uk/co/ractf/polaris/controller/replication/StaticReplicationControllerTest.java
+++ polaris-application/src/test/java/uk/co/ractf/polaris/controller/replication/StaticReplicationControllerTest.java
@@ -1,15 +1,12 @@
 package uk.co.ractf.polaris.controller.replication;
 
-import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import uk.co.ractf.polaris.api.deployment.StaticReplication;
 
 import java.util.ArrayList;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@ExtendWith(DropwizardExtensionsSupport.class)
 public class StaticReplicationControllerTest {
 
     @Test
