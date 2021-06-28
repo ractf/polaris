a881302dfcc058979a26cf2ca6b93a4a91af5b75
David <me@dave.lc>
2021-06-28 21:33:45 +0100

    add api url and token to namespace

--- polaris-api/src/main/java/uk/co/ractf/polaris/api/namespace/Namespace.java
+++ polaris-api/src/main/java/uk/co/ractf/polaris/api/namespace/Namespace.java
@@ -18,6 +18,8 @@ public class Namespace extends JsonRepresentable {
     private final Long allocatedMemory;
     private final List<String> allowedCapAdd;
     private final List<String> allowedCapDrop;
+    private final String apiUrl;
+    private final String apiToken;
 
     public Namespace(
             @JsonProperty("name") final String name,
@@ -27,7 +29,9 @@ public class Namespace extends JsonRepresentable {
             @JsonProperty("allocatecCpu") final Long allocatedCpu,
             @JsonProperty("allocatedMemory") final Long allocatedMemory,
             @JsonProperty("allowedCapAdd") final List<String> allowedCapAdd,
-            @JsonProperty("allowedCapDrop") final List<String> allowedCapDrop) {
+            @JsonProperty("allowedCapDrop") final List<String> allowedCapDrop,
+            @JsonProperty("apiUrl") final String apiUrl,
+            @JsonProperty("apiToken") final String apiToken) {
         this.name = name;
         this.hasResourceLimit = hasResourceLimit;
         this.maxCpu = maxCpu;
@@ -36,6 +40,8 @@ public class Namespace extends JsonRepresentable {
         this.allocatedMemory = allocatedMemory;
         this.allowedCapAdd = allowedCapAdd;
         this.allowedCapDrop = allowedCapDrop;
+        this.apiUrl = apiUrl;
+        this.apiToken = apiToken;
     }
 
     public String getName() {
@@ -70,16 +76,31 @@ public class Namespace extends JsonRepresentable {
         return allowedCapDrop;
     }
 
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public String getApiToken() {
+        return apiToken;
+    }
+
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
         if (!(o instanceof Namespace)) return false;
         final Namespace namespace = (Namespace) o;
-        return Objects.equals(name, namespace.name) && Objects.equals(hasResourceLimit, namespace.hasResourceLimit) && Objects.equals(maxCpu, namespace.maxCpu) && Objects.equals(maxMemory, namespace.maxMemory) && Objects.equals(allocatedCpu, namespace.allocatedCpu) && Objects.equals(allocatedMemory, namespace.allocatedMemory) && Objects.equals(allowedCapAdd, namespace.allowedCapAdd) && Objects.equals(allowedCapDrop, namespace.allowedCapDrop);
+        return Objects.equals(name, namespace.name) && Objects.equals(hasResourceLimit, namespace.hasResourceLimit) &&
+                Objects.equals(maxCpu, namespace.maxCpu) && Objects.equals(maxMemory, namespace.maxMemory) &&
+                Objects.equals(allocatedCpu, namespace.allocatedCpu) &&
+                Objects.equals(allocatedMemory, namespace.allocatedMemory) &&
+                Objects.equals(allowedCapAdd, namespace.allowedCapAdd) &&
+                Objects.equals(allowedCapDrop, namespace.allowedCapDrop) && Objects.equals(apiUrl, namespace.apiUrl) &&
+                Objects.equals(apiToken, namespace.apiToken);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, hasResourceLimit, maxCpu, maxMemory, allocatedCpu, allocatedMemory, allowedCapAdd, allowedCapDrop);
+        return Objects.hash(name, hasResourceLimit, maxCpu, maxMemory, allocatedCpu, allocatedMemory, allowedCapAdd,
+                allowedCapDrop, apiUrl, apiToken);
     }
 }
