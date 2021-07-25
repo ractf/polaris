e9bb2ef7f962cccd5336294ffafbe572c8993b00
David <me@dave.lc>
2021-07-25 16:30:47 +0100

    Fix tests

--- polaris-integration-tests/src/test/java/uk/co/ractf/polaris/tests/AuthenticationTest.java
+++ polaris-integration-tests/src/test/java/uk/co/ractf/polaris/tests/AuthenticationTest.java
@@ -10,7 +10,7 @@ public class AuthenticationTest extends IntegrationTest {
     @Test
     public void testRootCredentials() {
         final var apiClient = APIClient.create("http://127.0.0.1:8080", "polaris", "polaris");
-        assertEquals(apiClient.ping().exec().entrySet(), "polaris");
+        assertEquals("polaris", apiClient.ping().exec().get("username"));
     }
 
 }
