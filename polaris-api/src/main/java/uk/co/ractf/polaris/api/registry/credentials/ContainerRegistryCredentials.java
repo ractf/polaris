ef21cc9ffeba6a26da70e59a18a5f153408cf4a4
Ada Cooke <me@ada.rip>
2021-07-28 22:04:31 +0100

    Fix pod starting

--- polaris-api/src/main/java/uk/co/ractf/polaris/api/registry/credentials/ContainerRegistryCredentials.java
+++ polaris-api/src/main/java/uk/co/ractf/polaris/api/registry/credentials/ContainerRegistryCredentials.java
@@ -3,10 +3,12 @@ package uk.co.ractf.polaris.api.registry.credentials;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import uk.co.ractf.polaris.api.common.JsonRepresentable;
 import uk.co.ractf.polaris.api.namespace.NamespacedId;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", include = JsonTypeInfo.As.EXISTING_PROPERTY, visible = true)
 @JsonSubTypes({
         @JsonSubTypes.Type(value = AWSCredentials.class, name = "aws"),
         @JsonSubTypes.Type(value = StandardRegistryCredentials.class, name = "standard"),
