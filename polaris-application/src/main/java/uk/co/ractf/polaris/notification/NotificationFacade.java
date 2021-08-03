04b0f94d443bff1511162bd789e27cdd1868f04c
Ada Cooke <me@ada.rip>
2021-08-03 13:10:43 +0100

    Try to fix notification spam

--- polaris-application/src/main/java/uk/co/ractf/polaris/notification/NotificationFacade.java
+++ polaris-application/src/main/java/uk/co/ractf/polaris/notification/NotificationFacade.java
@@ -1,17 +1,34 @@
 package uk.co.ractf.polaris.notification;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.graph.Graph;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.jetbrains.annotations.NotNull;
 import uk.co.ractf.polaris.api.namespace.Namespace;
 import uk.co.ractf.polaris.api.notification.Notification;
 import uk.co.ractf.polaris.api.notification.NotificationLevel;
 import uk.co.ractf.polaris.api.notification.NotificationTarget;
 import uk.co.ractf.polaris.state.ClusterState;
 
+import java.util.concurrent.TimeUnit;
+
 @Singleton
 public class NotificationFacade {
 
     private final ClusterState state;
+
+    LoadingCache<String, String> recentlySent = CacheBuilder.newBuilder()
+            .maximumSize(100)
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .build(
+                    new CacheLoader<>() {
+                        public String load(final @NotNull String key) throws Exception {
+                            return key;
+                        }
+                    });
 
     @Inject
     public NotificationFacade(final ClusterState state) {
@@ -19,6 +36,10 @@ public class NotificationFacade {
     }
 
     public void notify(final Notification notification) {
+        if (recentlySent.getIfPresent(notification.getSummary()) != null) {
+            return;
+        }
+        recentlySent.put(notification.getSummary(), "");
         if (notification.getTarget() == NotificationTarget.SYSTEM_ADMIN) {
             for (final var receiver : state.getNotificationReceivers("polaris").values()) {
                 if (notification.getLevel().asInt() >= receiver.getMinimumLevel().asInt() &&
