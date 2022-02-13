1422d83f7c6d0e8ed039229a1b7aa01a25678269
Sam <tritoke@protonmail.com>
2022-02-13 19:13:43 +0000

    Added token expiry to CLI.

--- src/cmd/token/create.rs
+++ src/cmd/token/create.rs
@@ -1,6 +1,7 @@
 use crate::client::PolarisClient;
 use crate::cmd::APICommand;
 use crate::data::token::CreateableToken;
+use chrono::prelude::*;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -8,7 +9,10 @@ pub struct TokenCreate {
     #[clap(short, long, help = "Token Name")]
     name: String,
 
-    #[clap(short, long, help = "Token permissions")]
+    #[clap(short, long, help = "Token Expiry Date (RFC-3339 format)", parse(try_from_str = DateTime::parse_from_rfc3339))]
+    expiry: Option<DateTime<FixedOffset>>,
+
+    #[clap(short, long, help = "Token Permissions")]
     perms: String,
 }
 
@@ -16,12 +20,13 @@ pub struct TokenCreate {
 impl APICommand for TokenCreate {
     async fn run(&self, client: PolarisClient, json: bool) -> anyhow::Result<()> {
         let token_perms = self.perms.split(',').map(String::from).collect();
+        let token_expiry = self.expiry.map(Into::into);
 
         let token = client
             .create_token(CreateableToken {
                 name: self.name.clone(),
                 permissions: token_perms,
-                expiry: None,
+                expiry: token_expiry,
             })
             .await?;
         if json {
